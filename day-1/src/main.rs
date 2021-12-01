343e8adcec61ddde9805b39ee1e91324f79408f6
Harry Saunders <hsaunders1904@icloud.com>
2021-12-01 23:11:30 +0000

    Add test for error case in day-1 solution

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -33,9 +33,12 @@ fn read_lines<T: FromStr>(file_path: &str) -> Vec<Result<T, <T as FromStr>::Err>
         .collect()
 }
 
-fn count_increasing_numbers(values: &Vec<i32>, window_size: usize) -> u64 {
-    let mut count = 0;
+fn count_increasing_window_sums(values: &Vec<i32>, window_size: usize) -> u64 {
+    if window_size > values.len() {
+        return 0;
+    }
 
+    let mut count = 0;
     for idx in 0..(values.len() - window_size) {
         let window1 = &values[idx..(idx + window_size)];
         let window2 = &values[(idx + 1)..(idx + window_size + 1)];
@@ -58,11 +61,11 @@ fn main() {
 
     println!(
         "{} measurements were increasing with window size 1",
-        count_increasing_numbers(&depth_measurements, 1)
+        count_increasing_window_sums(&depth_measurements, 1)
     );
     println!(
         "{} measurements were increasing with window size 3",
-        count_increasing_numbers(&depth_measurements, 3)
+        count_increasing_window_sums(&depth_measurements, 3)
     );
 }
 
@@ -71,16 +74,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn count_increasing_numbers_counts_increasing_numbers_correctly() {
+    fn count_increasing_window_sums_correct_count_given_window_size_is_1() {
         let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        let num_increasing = count_increasing_numbers(&depths, 1);
+        let num_increasing = count_increasing_window_sums(&depths, 1);
         assert_eq!(num_increasing, 7);
     }
 
     #[test]
-    fn count_increasing_numbers_counts_correctly_with_3_value_sliding_window() {
+    fn count_increasing_window_sums_correct_count_given_window_size_is_3() {
         let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        let num_increasing = count_increasing_numbers(&depths, 3);
+        let num_increasing = count_increasing_window_sums(&depths, 3);
         assert_eq!(num_increasing, 5);
+    }
+
+    #[test]
+    fn count_increasing_window_sums_returns_0_given_window_size_gt_values_len() {
+        let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let num_increasing = count_increasing_window_sums(&depths, depths.len() + 1);
+        assert_eq!(num_increasing, 0);
     }
 }
