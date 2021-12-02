cd8c8e322d66a77440a55350e5570889d506a409
Harry Saunders <hsaunders1904@icloud.com>
2021-12-02 08:58:28 +0000

    Implement task 1 for day 2

---
+++ day-2/src/main.rs
@@ -0,0 +1,168 @@
+extern crate clap;
+use clap::{App, Arg};
+
+struct InputArgs {
+    file_path: String,
+}
+
+fn parse_args() -> InputArgs {
+    let matches = App::new("Advent of code day 1")
+        .version("0.0.1")
+        .about("Count the number of times a depth measurement increases")
+        .arg(
+            Arg::with_name("data_file")
+                .value_name("DATA_FILE")
+                .takes_value(true)
+                .help("Text file containing data - one integer per line")
+                .required(true),
+        )
+        .get_matches();
+
+    InputArgs {
+        file_path: matches.value_of("data_file").unwrap().to_owned(),
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum Direction {
+    Up,
+    Down,
+    Forward,
+}
+
+struct Instruction {
+    direction: Direction,
+    magnitude: i32,
+}
+
+impl From<&str> for Instruction {
+    fn from(instruction: &str) -> Instruction {
+        let parts: Vec<&str> = instruction.split(" ").collect();
+        if parts.len() > 2 {
+            panic!(
+                "More than one space in instruction string '{}'",
+                instruction
+            );
+        }
+        if let [direction_str, magnitude_str] = &parts[..] {
+            let direction = match direction_str.as_ref() {
+                "up" => Direction::Up,
+                "down" => Direction::Down,
+                "forward" => Direction::Forward,
+                _ => {
+                    panic!("Invalid direction string '{}'", direction_str)
+                }
+            };
+
+            let magnitude = magnitude_str.parse::<i32>().unwrap();
+
+            return Instruction {
+                direction: direction,
+                magnitude: magnitude,
+            };
+        }
+        panic!("No space found in instruction string '{}'", instruction);
+    }
+}
+
+fn parse_instructions(input: &str) -> Vec<Instruction> {
+    let mut instructions = Vec::new();
+    for line in input.lines() {
+        instructions.push(Instruction::from(line));
+    }
+
+    return instructions;
+}
+
+#[derive(Default, Debug)]
+struct Position {
+    horizontal: i32,
+    depth: i32,
+}
+
+impl Position {
+    fn new() -> Self {
+        Default::default()
+    }
+}
+
+fn accumulate_instructions(instructions: Vec<Instruction>) -> Position {
+    let mut pos = Position::new();
+    for instruction in instructions {
+        match instruction.direction {
+            Direction::Forward => pos.horizontal += instruction.magnitude,
+            Direction::Down => pos.depth += instruction.magnitude,
+            Direction::Up => pos.depth -= instruction.magnitude,
+        }
+    }
+    return pos;
+}
+
+fn main() {
+    let args = parse_args();
+
+    let file_contents = std::fs::read_to_string(args.file_path).expect("Error reading file.");
+    let instructions = parse_instructions(&file_contents);
+    let pos = accumulate_instructions(instructions);
+
+    println!("Final position: {:?}", pos);
+    println!(
+        "depth * horizontal position = {}",
+        pos.depth * pos.horizontal
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! Instruction_from_string_tests {
+        ($($name:ident: $value:expr,)*) => {$(
+            #[test]
+            fn $name() {
+                let (input, expected_direction, expected_magnitude) = $value;
+                let instruction = Instruction::from(input);
+
+                assert_eq!(instruction.direction, expected_direction);
+                assert_eq!(instruction.magnitude, expected_magnitude);
+            }
+        )*}
+    }
+    Instruction_from_string_tests! {
+        up_4: ("up 4", Direction::Up, 4),
+        down_2: ("down 2", Direction::Down, 2),
+        forward_negative_10: ("forward -10", Direction::Forward, -10),
+    }
+
+    #[test]
+    fn parse_instructions_generates_instructions() {
+        let instructions_str = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2";
+
+        let instructions = parse_instructions(&instructions_str);
+
+        assert_eq!(instructions.len(), 6);
+        assert_eq!(instructions[0].direction, Direction::Forward);
+        assert_eq!(instructions[1].direction, Direction::Down);
+        assert_eq!(instructions[2].direction, Direction::Forward);
+        assert_eq!(instructions[3].direction, Direction::Up);
+        assert_eq!(instructions[4].direction, Direction::Down);
+        assert_eq!(instructions[5].direction, Direction::Forward);
+        assert_eq!(instructions[0].magnitude, 5);
+        assert_eq!(instructions[1].magnitude, 5);
+        assert_eq!(instructions[2].magnitude, 8);
+        assert_eq!(instructions[3].magnitude, 3);
+        assert_eq!(instructions[4].magnitude, 8);
+        assert_eq!(instructions[5].magnitude, 2);
+    }
+
+    #[test]
+    fn positions_are_correctly_accumulated() {
+        let instructions_str = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2";
+        let instructions = parse_instructions(&instructions_str);
+
+        let pos = accumulate_instructions(instructions);
+
+        assert_eq!(pos.depth, 10);
+        assert_eq!(pos.horizontal, 15);
+    }
+}
