8cee834be330e0b2f5c77b3610ee47a0cf8d5b8e
Harry Saunders <hsaunders1904@icloud.com>
2021-12-07 00:17:02 +0000

    Complete task 2 for day 3

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,5 +1,6 @@
 mod cli_parser;
 
+use bit_field::BitField;
 use std::io::BufRead;
 
 fn read_lines_to_decimal<T>(lines: std::io::Lines<T>) -> Vec<u64>
@@ -22,10 +23,9 @@ where
 // Count the number of 1s in each bit position in each value
 fn get_bit_counts(values: &Vec<u64>, num_bits: usize) -> Vec<u64> {
     let mut counts: Vec<u64> = vec![0; num_bits];
-    for value in values {
+    for &value in values {
         for bit_idx in 0..num_bits {
-            let filter = 1 << (num_bits - bit_idx - 1);
-            if value & filter == filter {
+            if value.get_bit(num_bits - bit_idx - 1) {
                 counts[bit_idx] += 1;
             }
         }
@@ -33,26 +33,80 @@ fn get_bit_counts(values: &Vec<u64>, num_bits: usize) -> Vec<u64> {
     return counts;
 }
 
-fn calculate_consumption(values: Vec<u64>, num_bits: usize) -> u64 {
+fn calculate_consumption(values: &Vec<u64>, num_bits: usize) -> u64 {
     let bit_counts = get_bit_counts(&values, num_bits);
 
     let mut gamma_rate: u64 = 0;
     let mut epsilon: u64 = 0;
-    for count in &bit_counts {
-        if *count as f64 / values.len() as f64 > 0.5 {
-            gamma_rate += 1;
-        } else {
-            epsilon += 1;
-        }
-        gamma_rate <<= 1;
-        epsilon <<= 1;
+    for (bit_idx, count) in bit_counts.iter().enumerate() {
+        let most_common_bit = *count as f64 / values.len() as f64 > 0.5;
+        gamma_rate.set_bit(num_bits - bit_idx - 1, most_common_bit);
+        epsilon.set_bit(num_bits - bit_idx - 1, !most_common_bit);
     }
-    gamma_rate >>= 1;
-    epsilon >>= 1;
-
     println!("gamma_rate: {}", gamma_rate);
     println!("epsilon: {}", epsilon);
     return gamma_rate * epsilon;
+}
+
+fn count_ones_in_bit_pos(values: &Vec<u64>, bit_pos: usize) -> usize {
+    let mut count: usize = 0;
+    for value in values {
+        if value.get_bit(bit_pos) {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn get_most_common_bit(values: &Vec<u64>, bit_pos: usize) -> bool {
+    let ones_count = count_ones_in_bit_pos(values, bit_pos);
+    ones_count as f64 >= 0.5 * values.len() as f64
+}
+
+fn get_oxygen_rating(mut values: Vec<u64>, num_bits: usize) -> u64 {
+    let mut bit_idx = num_bits;
+    while values.len() > 1 {
+        bit_idx -= 1;
+        let most_common_bit = get_most_common_bit(&values, bit_idx);
+        values = values
+            .into_iter()
+            .filter_map(|x| {
+                if x.get_bit(bit_idx) == most_common_bit {
+                    Some(x)
+                } else {
+                    None
+                }
+            })
+            .collect();
+    }
+    values[0]
+}
+
+fn get_co2_rating(mut values: Vec<u64>, num_bits: usize) -> u64 {
+    let mut bit_idx = num_bits;
+    while values.len() > 1 {
+        bit_idx -= 1;
+        let most_common_bit = get_most_common_bit(&values, bit_idx);
+        values = values
+            .into_iter()
+            .filter_map(|x| {
+                if x.get_bit(bit_idx) == most_common_bit {
+                    None
+                } else {
+                    Some(x)
+                }
+            })
+            .collect();
+    }
+    values[0]
+}
+
+fn get_life_support_rating(values: &Vec<u64>, num_bits: usize) -> u64 {
+    let oxygen_rating = get_oxygen_rating(values.clone(), num_bits);
+    println!("oxygen_rating: {}", oxygen_rating);
+    let co2_rating = get_co2_rating(values.clone(), num_bits);
+    println!("co2_rating: {}", co2_rating);
+    oxygen_rating * co2_rating
 }
 
 fn main() {
@@ -67,15 +121,22 @@ fn main() {
         Ok(file) => file,
     };
 
+    let num_bits = 12;
     let reader = std::io::BufReader::new(file);
     let values = read_lines_to_decimal(reader.lines());
-    let consumption = calculate_consumption(values, 12);
+    let consumption = calculate_consumption(&values, num_bits);
     println!("Power consumption: {}", consumption);
+
+    let ls_rating = get_life_support_rating(&values, num_bits);
+    println!("Life support rating: {}", ls_rating);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const DIAG_REPORT: &str = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n\
+            11100\n10000\n11001\n00010\n01010";
 
     fn read_from_string(s: &str) -> &[u8] {
         s.as_bytes()
@@ -83,12 +144,41 @@ mod tests {
 
     #[test]
     fn correct_power_consumption() {
-        let diag_report =
-            "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
-        let buf = std::io::BufReader::new(read_from_string(diag_report));
+        let buf = std::io::BufReader::new(read_from_string(DIAG_REPORT));
         let values = read_lines_to_decimal(buf.lines());
-        let power_consumption = calculate_consumption(values, 5);
+
+        let power_consumption = calculate_consumption(&values, 5);
 
         assert_eq!(power_consumption, 198);
+    }
+
+    #[test]
+    fn correct_oxygen_rating() {
+        let buf = std::io::BufReader::new(read_from_string(DIAG_REPORT));
+        let values = read_lines_to_decimal(buf.lines());
+
+        let o_ratio = get_oxygen_rating(values, 5);
+
+        assert_eq!(o_ratio, 23);
+    }
+
+    #[test]
+    fn correct_co2_rating() {
+        let buf = std::io::BufReader::new(read_from_string(DIAG_REPORT));
+        let values = read_lines_to_decimal(buf.lines());
+
+        let co2_ratio = get_co2_rating(values, 5);
+
+        assert_eq!(co2_ratio, 10);
+    }
+
+    #[test]
+    fn correct_life_support_rating() {
+        let buf = std::io::BufReader::new(read_from_string(DIAG_REPORT));
+        let values = read_lines_to_decimal(buf.lines());
+
+        let ls_rating = get_life_support_rating(&values, 5);
+
+        assert_eq!(ls_rating, 230);
     }
 }
